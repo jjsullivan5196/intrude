10bf7e438c724a78a3e2398066e43f203f434adf
John Sullivan <jsullivan@csumb.edu>
2016-05-08 17:45:59 -0700

    Initial Commit

---
+++ criminals.R
@@ -0,0 +1,28 @@
+dat = read.csv("vcdb.csv")
+dat = dat[,names(dat) != "X"]
+dat = dat[,-grep("action.environmental", colnames(dat))]
+dat = dat[,-grep("action.error", colnames(dat))]
+dat = dat[,-grep("action.hacking", colnames(dat))]
+dat = dat[,-grep("action.malware", colnames(dat))]
+dat = dat[,-grep("action.misuse", colnames(dat))]
+dat = dat[,-grep("action.physical", colnames(dat))]
+dat = dat[,-grep("action.social", colnames(dat))]
+dat = dat[,-grep("action.unknown", colnames(dat))]
+dat = dat[,-grep("actor", colnames(dat))]
+dat = dat[,-grep("asset", colnames(dat))]
+dat = dat[,-grep("attribute", colnames(dat))]
+dat = dat[,-grep("campaign", colnames(dat))]
+dat = dat[,-grep("confidence", colnames(dat))]
+dat = dat[,-grep("control", colnames(dat))]
+dat = dat[,-grep("corrective", colnames(dat))]
+dat = dat[,-grep("cost", colnames(dat))]
+dat = dat[,-grep("discovery", colnames(dat))]
+dat = dat[,-grep("impact", colnames(dat))]
+dat = dat[,-grep("plus", colnames(dat))]
+dat = dat[,-grep("targeted", colnames(dat))]
+
+vic.countries = dat[,grep("victim.country.", colnames(dat))]
+vic.countries$country = apply(vic.countries, 1, function(x) return(substring(colnames(vic.countries)[which(x == TRUE)], 16)))
+dat = dat[,-grep("victim.country.", colnames(dat))]
+dat$victim.country = vic.countries$country
+
