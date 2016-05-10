337f8a7649fe209ab90cab4cd9f81608a73b37f3
John Sullivan <jsullivan@csumb.edu>
2016-05-10 14:50:02 -0700

    added transform function

--- criminals.R
+++ criminals.R
@@ -21,8 +21,17 @@ dat = dat[,-grep("impact", colnames(dat))]
 dat = dat[,-grep("plus", colnames(dat))]
 dat = dat[,-grep("targeted", colnames(dat))]
 
-vic.countries = dat[,grep("victim.country.", colnames(dat))]
-vic.countries$country = apply(vic.countries, 1, function(x) return(substring(colnames(vic.countries)[which(x == TRUE)], 16)))
-dat = dat[,-grep("victim.country.", colnames(dat))]
-dat$victim.country = vic.countries$country
+#vic.countries = dat[,grep("victim.country.", colnames(dat))]
+#vic.countries$country = apply(vic.countries, 1, function(x) return(substring(colnames(vic.countries)[which(x == TRUE)], 16)))
+#dat = dat[,-grep("victim.country.", colnames(dat))]
+#dat$victim.country = vic.countries$country
+
+dat$victim.country = transform_row(dat, "victim.country.")
+
+tranform_row = function(dat, name)
+{
+  subframe = dat[,grep(name, colnames(dat))]
+  sub = apply(subframe, 1, function(x) return(substring(colnames(subframe)[which(x == TRUE)], nchar(name) + 1)))
+  return(sub)
+}
 
